47fed89bf8a32cab4e3e84b469c692b750de93df
tradertradelens-dotcom <tradertradelens@gmail.com>
2026-01-01 23:19:36 -0300

    Delete Journal.tsx

--- Journal.tsx
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-import { JournalEntry } from '../types';
-
-const Journal: React.FC<{ entries: JournalEntry[], onUpdate: any }> = ({ entries }) => {
-  return <div>Journal Rendered</div>;
-};
-export default Journal;
